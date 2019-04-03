85f08e820fb2b388be925f250295477376270671
sigoden <sigoden@126.com>
2019-04-03 14:09:49 +0800

    Update .github/action-deploy/entrypoint.sh

--- .github/action-deploy/entrypoint.sh
+++ .github/action-deploy/entrypoint.sh
@@ -8,7 +8,12 @@ if [ -z "$RENDER_BRANCH" ]; then
     echo "Set the RENDER_BRANCH env variable."
     exit 1
 fi
-
+if [ -n "${GIT_DEPLOY_KEY}" ]; then
+    mkdir /root/.ssh
+    ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
+    echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
+    chmod 400 /root/.ssh/id_rsa
+fi
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
