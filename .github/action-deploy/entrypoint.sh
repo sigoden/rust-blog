37a32f704c1dddc831ee8537c7e4555177e74f18
sigoden <sigoden@gmail.com>
2019-04-03 14:57:31 +0800

    update github action

--- .github/action-deploy/entrypoint.sh
+++ .github/action-deploy/entrypoint.sh
@@ -2,7 +2,7 @@
 
 STAGE_DIR=${GITHUB_WORKSPACE}/stage
 GHPAGES_BRANCH=gh-pages
-TARGET_REPO_URL="git@github.com/${GITHUB_REPOSITORY}.git"
+TARGET_REPO_URL="git@github.com:${GITHUB_REPOSITORY}.git"
 
 if [ -z "$RENDER_BRANCH" ]; then
     echo "Set the RENDER_BRANCH env variable."
@@ -19,7 +19,7 @@ git submodule init
 git submodule update --remote --recursive
 echo "cloned the repo"
 
-npm install
+npm install --silent
 npx gatsby build
 echo "built with gatsby"
 
@@ -33,6 +33,6 @@ git checkout --orphan $GHPAGES_BRANCH
 git add .
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git commit -m "Automated deployment to GitHub Pages"
+git commit -q -m "Automated deployment to GitHub Pages"
 git push deploy $GHPAGES_BRANCH --force
 echo "deployed to gh pages"
